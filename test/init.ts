c20e36dbe831a06f3c6de345b5602f0b10560506
Jeff Huang <jeff@dinngo.co>
2024-01-19 15:28:22 +0800

    fix: init.ts rpc url

--- test/init.ts
+++ test/init.ts
@@ -1,3 +1,9 @@
 import * as common from '@protocolink/common';
 
-common.setNetwork(common.ChainId.mainnet, { rpcUrl: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161' });
+if (process.env.CHAIN_ID && process.env.HTTP_RPC_URL) {
+  common.setNetwork(parseInt(process.env.CHAIN_ID), { rpcUrl: process.env.HTTP_RPC_URL });
+} else if (process.env.MAINNET_RPC_URL) {
+  common.setNetwork(common.ChainId.mainnet, { rpcUrl: process.env.MAINNET_RPC_URL });
+} else {
+  common.setNetwork(common.ChainId.mainnet, { rpcUrl: 'https://eth.llamarpc.com' });
+}
