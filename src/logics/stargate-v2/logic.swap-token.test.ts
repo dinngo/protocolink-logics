e7e50fd6492018f8e212e8b9e52100627bb80c89
Jeff Huang <jeff@dinngo.co>
2024-08-15 17:31:00 +0800

    fix: disable native and stable coins in Stargate V2, add cake between bnb and polygon zkevm

--- src/logics/stargate-v2/logic.swap-token.test.ts
+++ src/logics/stargate-v2/logic.swap-token.test.ts
@@ -1,11 +1,12 @@
 import { LogicTestCase } from 'test/types';
-import { StargatePool__factory } from './contracts';
+import { OFTWrapper__factory, StargatePool__factory } from './contracts';
 import { SwapTokenLogic, SwapTokenLogicFields, SwapTokenLogicOptions } from './logic.swap-token';
+import { bnbTokens, polygonZkevmTokens } from './tokens';
 import * as common from '@protocolink/common';
 import { constants, utils } from 'ethers';
 import * as core from '@protocolink/core';
 import { expect } from 'chai';
-import { getPoolByTokenAddress } from './configs';
+import { getPoolConfigByTokenAddress } from './configs';
 
 describe('StargateV2 SwapTokenLogic', function () {
   context('Test getTokenList', async function () {
@@ -19,62 +20,93 @@ describe('StargateV2 SwapTokenLogic', function () {
   });
 
   context('Test build', function () {
-    const chainId = common.ChainId.mainnet;
-    const logic = new SwapTokenLogic(chainId);
-    const stgIface = StargatePool__factory.createInterface();
+    const stgIface = OFTWrapper__factory.createInterface();
     const account = '0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa';
 
     const testCases: LogicTestCase<SwapTokenLogicFields, SwapTokenLogicOptions>[] = [
       {
         fields: {
-          input: new common.TokenAmount(common.mainnetTokens.ETH, '1'),
-          output: new common.TokenAmount(common.optimismTokens.ETH, '1'),
+          input: new common.TokenAmount(bnbTokens.CAKE, '1'),
+          output: new common.TokenAmount(polygonZkevmTokens.CAKE, '1'),
           receiver: account,
           fee: '0',
-          lzTokenFee: '0',
         },
         options: { account },
       },
       {
         fields: {
-          input: new common.TokenAmount(common.mainnetTokens.USDC, '1'),
-          output: new common.TokenAmount(common.optimismTokens.USDC, '1'),
+          input: new common.TokenAmount(polygonZkevmTokens.CAKE, '1'),
+          output: new common.TokenAmount(bnbTokens.CAKE, '1'),
           receiver: account,
-          fee: '0.01',
-          lzTokenFee: '0.01',
+          fee: '0.1',
         },
         options: { account },
       },
       {
         fields: {
-          input: new common.TokenAmount(common.mainnetTokens.USDC, '1'),
-          output: new common.TokenAmount(common.optimismTokens.USDC, '1'),
+          input: new common.TokenAmount(polygonZkevmTokens.CAKE, '1'),
+          output: new common.TokenAmount(bnbTokens.CAKE, '1'),
           receiver: account,
-          fee: '0.01',
-          lzTokenFee: '0.01',
+          fee: '0.1',
           balanceBps: 5000,
         },
         options: { account },
       },
+      // {
+      //   fields: {
+      //     input: new common.TokenAmount(common.mainnetTokens.ETH, '1'),
+      //     output: new common.TokenAmount(common.optimismTokens.ETH, '1'),
+      //     receiver: account,
+      //     fee: '0',
+      //     lzTokenFee: '0',
+      //   },
+      //   options: { account },
+      // },
+      // {
+      //   fields: {
+      //     input: new common.TokenAmount(common.mainnetTokens.USDC, '1'),
+      //     output: new common.TokenAmount(common.optimismTokens.USDC, '1'),
+      //     receiver: account,
+      //     fee: '0.01',
+      //     lzTokenFee: '0.01',
+      //   },
+      //   options: { account },
+      // },
+      // {
+      //   fields: {
+      //     input: new common.TokenAmount(common.mainnetTokens.USDC, '1'),
+      //     output: new common.TokenAmount(common.optimismTokens.USDC, '1'),
+      //     receiver: account,
+      //     fee: '0.01',
+      //     lzTokenFee: '0.01',
+      //     balanceBps: 5000,
+      //   },
+      //   options: { account },
+      // },
     ];
 
     testCases.forEach(({ fields, options }) => {
       it(`${fields.input.token.symbol} to ${fields.output.token.symbol}`, async function () {
+        const logic = new SwapTokenLogic(fields.input.token.chainId);
         const routerLogic = await logic.build(fields, options);
         const sig = routerLogic.data.substring(0, 10);
         const { input, balanceBps, fee } = fields;
-        const poolAddress = getPoolByTokenAddress(chainId, input.token.address);
+        const pool = getPoolConfigByTokenAddress(input.token.chainId, input.token.address);
 
-        expect(routerLogic.to).to.eq(poolAddress);
-        expect(sig).to.eq(stgIface.getSighash('sendToken'));
+        expect(routerLogic.to).to.eq(pool.address);
+        if (pool.proxyOFT) {
+          expect(sig).to.eq(stgIface.getSighash('sendProxyOFTFeeV2'));
+        } else {
+          expect(sig).to.eq(stgIface.getSighash('sendOFTFeeV2'));
+        }
         expect(routerLogic.inputs[0].token).to.eq(input.token.elasticAddress);
-        expect(routerLogic.inputs[1].token).to.eq(common.mainnetTokens.ETH.elasticAddress);
+        expect(routerLogic.inputs[1].token).to.eq(common.getNativeToken(input.token.chainId).elasticAddress);
         expect(routerLogic.inputs[1].amountOrOffset).to.eq(common.toSmallUnit(fee, common.mainnetTokens.ETH.decimals));
         expect(utils.isBytesLike(routerLogic.data)).to.be.true;
 
         if (balanceBps) {
           expect(routerLogic.inputs[0].balanceBps).to.eq(balanceBps);
-          expect(routerLogic.inputs[0].amountOrOffset).to.eq(common.getParamOffset(2));
+          expect(routerLogic.inputs[0].amountOrOffset).to.eq(common.getParamOffset(3));
         } else {
           expect(routerLogic.inputs[0].balanceBps).to.eq(core.BPS_NOT_USED);
           expect(routerLogic.inputs[0].amountOrOffset).eq(input.amountWei);
---
+++ test/logics/stargate-v2/swap-token-polygonZkevm.test.ts
@@ -0,0 +1,91 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { claimToken, getChainId, snapshotAndRevertEach } from '@protocolink/test-helpers';
+import * as common from '@protocolink/common';
+import * as core from '@protocolink/core';
+import { expect } from 'chai';
+import { getNativeToken } from '@protocolink/common';
+import hre from 'hardhat';
+import { polygonZkevmTokens } from 'src/logics/stargate-v2/tokens';
+import * as stargate from 'src/logics/stargate-v2';
+import * as utils from 'test/utils';
+
+describe('polygonZkevm: Test StargateV2 SwapToken Logic', function () {
+  let chainId: number;
+  let user: SignerWithAddress;
+
+  before(async function () {
+    chainId = await getChainId();
+    [, user] = await hre.ethers.getSigners();
+    await claimToken(
+      chainId,
+      user.address,
+      polygonZkevmTokens.CAKE,
+      '10',
+      '0xb4BAB40e5a869eF1b5ff440a170A57d9feb228e9'
+    );
+  });
+
+  snapshotAndRevertEach();
+
+  const testCases = [
+    {
+      params: {
+        input: new common.TokenAmount(polygonZkevmTokens.CAKE, '1'),
+        tokenOut: common.bnbTokens.Cake,
+      },
+    },
+    {
+      params: {
+        input: new common.TokenAmount(polygonZkevmTokens.CAKE, '1'),
+        tokenOut: common.bnbTokens.Cake,
+      },
+      balanceBps: 5000,
+    },
+  ];
+
+  testCases.forEach(({ params, balanceBps }, i) => {
+    it(`case ${i + 1}`, async function () {
+      // 1. get input or output
+      const stargateSwapTokenLogic = new stargate.SwapTokenLogic(chainId);
+      const quotation = await stargateSwapTokenLogic.quote({ receiver: user.address, ...params });
+      const { input, fee } = quotation;
+
+      // 2. build funds, tokensReturn
+      const feeTokenAmount = new common.TokenAmount(getNativeToken(chainId), fee);
+      const funds = new common.TokenAmounts([feeTokenAmount]);
+      const tokensReturn = [input.token.elasticAddress];
+      if (balanceBps) {
+        funds.add(utils.calcRequiredAmountByBalanceBps(input, balanceBps));
+        tokensReturn.push(input.token.elasticAddress);
+      } else {
+        funds.add(input);
+      }
+
+      // 3. build router logics
+      const routerLogics: core.DataType.LogicStruct[] = [];
+      routerLogics.push(await stargateSwapTokenLogic.build(quotation, { account: user.address }));
+
+      // 4. get router permit2 datas
+      let permit2Datas;
+      if (!input.token.isNative) {
+        permit2Datas = await utils.getRouterPermit2Datas(chainId, user, funds.erc20);
+      }
+      // 5. send router tx
+      const routerKit = new core.RouterKit(chainId);
+      const transactionRequest = routerKit.buildExecuteTransactionRequest({
+        permit2Datas,
+        routerLogics,
+        tokensReturn,
+        value: funds.native?.amountWei ?? 0,
+      });
+
+      await expect(user.sendTransaction(transactionRequest)).to.not.be.reverted;
+      if (input.token.isNative) {
+        await expect(user.address).to.changeBalance(getNativeToken(chainId), -input.add(fee).amount, 1);
+      } else {
+        await expect(user.address).to.changeBalance(input.token, -input.amount);
+        await expect(user.address).to.changeBalance(getNativeToken(chainId), -fee);
+      }
+    });
+  });
+});
